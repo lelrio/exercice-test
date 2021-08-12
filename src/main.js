6381057f9e0423170fb13ef64cf3526400f40383
lelrio <34245393+lelrio@users.noreply.github.com>
2021-08-12 14:57:25 +0200

    Add files via upload
    presque terminé

--- src/main.js
+++ src/main.js
@@ -14,8 +14,10 @@ async function getBeers(){
 
     const container = document.getElementsByClassName('container')[0];
     const position = document.getElementsByClassName('position')[0];
+
     
     container.appendChild(position);
+
 
     for(var i = 0 ; i < obj.length ; i++){
       
@@ -24,16 +26,78 @@ async function getBeers(){
         var btn = document.createElement('button');
         var img = document.createElement('img');
         var divBeers = document.createElement('div');
+        var modalImg = document.createElement('img')
+        var modalTitle = document.createElement('h1');
+        var modalTagline = document.createElement('h3')
+        var modalDescription = document.createElement('p')
+        var modalAbv = document.createElement('h5')
+        var modalIbu = document.createElement('h5') 
+        var modalEbcSrm = document.createElement('h5') 
+        var modalMalt = document.createElement('h5') 
+        var modalYeast = document.createElement('h5') 
 
+        modalTitle.textContent = obj[i].name
+        modalTagline.textContent = obj[i].tagline
+        modalDescription.textContent = obj[i].description
+        modalAbv.innerHTML = "ABV" + obj[i].abv
+        modalIbu.innerHTML = "Ibu" + obj[i].ibu
+        modalEbcSrm.textContent ="Ebc/Srm" +  obj[i].ebc + "/" + obj[i].srm
+
+        var text = "";
+        for(var j = 0; j < obj[i].ingredients.malt.length; j++){
+          text += obj[i].ingredients.malt[j].name + "<br>"
+        }
+        modalMalt.innerHTML = text
+        modalYeast.textContent= obj[i].ingredients.yeast
+
+        
+        var modal = document.createElement("modal");
+        modal.classList.add("modal");
+        var contain = document.createElement('contain');
+        var contenuLeft = document.createElement('contenuLeft')
+        var contenuRight = document.createElement('contenuRight')
+        var closeBtn = document.createElement('span');
+        closeBtn.innerHTML = "X"
+        closeBtn.classList.add('close-btn')
+        contain.classList.add('contain')
+
+        modal.appendChild(contain);
+        contain.appendChild(contenuLeft)
+        contain.appendChild(contenuRight);
+        contenuLeft.appendChild(modalImg);
+        contenuRight.appendChild(modalTagline);
+        contenuRight.appendChild(modalDescription);
+        contenuRight.appendChild(modalAbv);
+        contenuRight.appendChild(modalIbu);
+        contenuRight.appendChild(modalEbcSrm);
+        contenuRight.appendChild(modalMalt);
+        contenuRight.appendChild(modalYeast);
+
+        contenuLeft.appendChild(closeBtn);
+
+        contenuLeft.classList.add("leftImage")
+        contenuRight.classList.add("rightText")
+
+        btn.onclick = function(){
+          document.getElementById("modal" + this.id).style.display = "block"
+        }
+        closeBtn.onclick = function(){
+          document.getElementById("modal" + this.id).style.display = "none"
+        }
+    
         var divText= document.createElement('div')
         var divImg = document.createElement('div')
 
         h1.classList.add("title")
         p.classList.add("tagline")
-        btn.setAttribute("id", "btn");
+        btn.classList.add("btn")
+        btn.setAttribute("id", i);
+        modal.setAttribute("id", "modal" + i)
+        closeBtn.setAttribute("id", i)
         divBeers.classList.add("divBeers")
         divText.classList.add("divText")
         divImg.classList.add("divImg")
+        modalImg.classList.add('modalImg')
         
         position.appendChild(divBeers);
 
@@ -51,7 +115,6 @@ async function getBeers(){
        
         h1.textContent = obj[i].name
         btn.innerHTML = "see more"
-        btn.textContent = obj[i].id;
         p.textContent = obj[i].tagline
         img.src= obj[i].image_url
 
@@ -59,36 +122,10 @@ async function getBeers(){
         divText.appendChild(p);
         divText.appendChild(btn);
         divImg.appendChild(img);
-    }
-    
-    let modalBtn = document.getElementById("btn")
-    let modal = document.querySelector(".modal")
-    let closeBtn = document.querySelector(".close-btn")
-    modalBtn.onclick = function(){
-      modal.style.display = "block"
-      const contain = document.getElementsByClassName('contain')[0];
-      const contenuLeft = document.getElementsByClassName('contenuLeft')[0];
-      const contenuRight = document.getElementsByClassName('contenuRight')[0];
-      contain.appendChild(contenuLeft);
-      contain.appendChild(contenuRight);
-      contenuLeft.appendChild(img);
-      contenuRight.appendChild(h1);
-      contenuLeft.classList.add("leftImage")
-      contenuRight.classList.add("rightText")
-      
-    }
-    closeBtn.onclick = function(){
-      modal.style.display = "none"
-    }
-    window.onclick = function(e){
-      if(e.target == modal){
-        modal.style.display = "none"
-      }
-    }
 
-  
-    
-    
+        divBeers.appendChild(modal);
+
+    }
 }
 
 
